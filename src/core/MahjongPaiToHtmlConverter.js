20cfc13fbac0ba7ded17bf5f21c2c884c297e18f
matsumatsu233 <cuizhen199209@gmail.com>
2017-11-18 16:35:43 +0900

    Use space instead of | as spliter

--- src/core/MahjongPaiToHtmlConverter.js
+++ src/core/MahjongPaiToHtmlConverter.js
@@ -5,23 +5,23 @@ export function convert(outputSet, options) {
 
   outputSet.forEach((tile) => {
     if (isSpace(tile)) {
-      outputHtml += `<span style='margin-right:${10 * scale}px'></span>`;
-    } else if (isSpilter(tile)) {
-      outputHtml += `<span style='margin-right:${30 * scale}px'></span>`;
+      outputHtml += `<span title='test' style='margin-right:${10 * scale}px'>&nbsp;</span>`;
+    } else if (isTwoSpaces(tile)) {
+      outputHtml += `<span style='margin-right:${30 * scale}px'>&nbsp;&nbsp;</span>`;
     } else if (isKan(tile)) {
-      tile = tile[0].toLowerCase() + tile.substring(1);
+      let srcTile = tile[0].toLowerCase() + tile.substring(1);
       outputHtml += `<span style='display: inline-block; height:${60 * scale}px'>`;
-      outputHtml += `<img style='display: block' src='${url + tile}.gif' height='${30 * scale}px'>`;
-      outputHtml += `<img style='display: block' src='${url + tile}.gif' height='${30 * scale}px'>`;
+      outputHtml += `<img style='display: block' alt='${tile.slice(0,2)}' src='${url + srcTile}.gif' height='${30 * scale}px'>`;
+      outputHtml += `<img style='display: block' alt='${tile.slice(1,3)}' src='${url + srcTile}.gif' height='${30 * scale}px'>`;
       outputHtml += "</span>";
     } else if (isRotation(tile)) {
       if (tile[0] === "b") {
-        outputHtml += `<img src='${url + tile}.gif' height='${40 * scale}px'>`;
+        outputHtml += `<img src='${url}ura.gif' alt='${tile}' height='${40 * scale}px'>`;
       } else {
-        outputHtml += `<img src='${url + tile}.gif' height='${30 * scale}px'>`;
+        outputHtml += `<img src='${url + tile}.gif' alt='${tile}' height='${30 * scale}px'>`;
       }
     } else {
-      outputHtml += `<img src='${url + tile}.gif' height='${40 * scale}px'>`;
+      outputHtml += `<img src='${url + tile}.gif' alt='${tile}' height='${40 * scale}px'>`;
     }
   });
 
@@ -32,8 +32,8 @@ function isSpace(tile) {
   return tile === " ";
 }
 
-function isSpilter(tile) {
-  return tile === "|";
+function isTwoSpaces(tile) {
+  return tile === "  ";
 }
 
 function isKan(tile) {
