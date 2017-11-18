4ef6b17bbd13bf8898881acdde37467d8ffb2f23
matsumatsu233 <cuizhen199209@gmail.com>
2017-11-18 14:28:49 +0900

    Change to use template string

--- src/core/MahjongPaiToHtmlConverter.js
+++ src/core/MahjongPaiToHtmlConverter.js
@@ -3,45 +3,45 @@ export function convert(outputSet, options) {
   let scale = getScaleFromSize(options.size);
   let outputHtml = "";
 
-  outputSet.forEach((pai) => {
-    if (isSpace(pai)) {
-      outputHtml += "<span style='margin-right:" + (10 * scale) + "px'></span>";
-    } else if (isSpilter(pai)) {
-      outputHtml += "<span style='margin-right:" + (30 * scale) + "px'></span>";
-    } else if (isKan(pai)) {
-      pai = pai[0].toLowerCase() + pai.substring(1);
-      outputHtml += "<span style='display: inline-block; height: " + (60 * scale) + "px'>";
-      outputHtml += "<img style='display: block' src='" + url + pai + ".gif' height='" + (30 * scale) + "px'>";
-      outputHtml += "<img style='display: block' src='" + url + pai + ".gif' height='" + (30 * scale) + "px'>";
+  outputSet.forEach((tile) => {
+    if (isSpace(tile)) {
+      outputHtml += `<span style='margin-right:${10 * scale}px'></span>`;
+    } else if (isSpilter(tile)) {
+      outputHtml += `<span style='margin-right:${30 * scale}px'></span>`;
+    } else if (isKan(tile)) {
+      tile = tile[0].toLowerCase() + tile.substring(1);
+      outputHtml += `<span style='display: inline-block; height:${60 * scale}px'>`;
+      outputHtml += `<img style='display: block' src='${url + tile}.gif' height='${30 * scale}px'>`;
+      outputHtml += `<img style='display: block' src='${url + tile}.gif' height='${30 * scale}px'>`;
       outputHtml += "</span>";
-    } else if (isRotation(pai)) {
-      if (pai[0] === "b") {
-        outputHtml += "<img src='" + url + "ura.gif' height='" + (40 * scale) + "px'>";
+    } else if (isRotation(tile)) {
+      if (tile[0] === "b") {
+        outputHtml += `<img src='${url + tile}.gif' height='${40 * scale}px'>`;
       } else {
-        outputHtml += "<img src='" + url + pai + ".gif' height='" + (30 * scale) + "px'>";
+        outputHtml += `<img src='${url + tile}.gif' height='${30 * scale}px'>`;
       }
     } else {
-      outputHtml += "<img src='" + url + pai + ".gif' height='" + (40 * scale) + "px'>";
+      outputHtml += `<img src='${url + tile}.gif' height='${40 * scale}px'>`;
     }
   });
 
   return outputHtml;
 }
 
-function isSpace(pai) {
-  return pai === " ";
+function isSpace(tile) {
+  return tile === " ";
 }
 
-function isSpilter(pai) {
-  return pai === "|";
+function isSpilter(tile) {
+  return tile === "|";
 }
 
-function isKan(pai) {
-  return pai[0] === "L" || pai[0] === "R";
+function isKan(tile) {
+  return tile[0] === "L" || tile[0] === "R";
 }
 
-function isRotation(pai) {
-  return pai[0] === "l" || pai[0] === "r" || pai[0] === "b";
+function isRotation(tile) {
+  return tile[0] === "l" || tile[0] === "r" || tile[0] === "b";
 }
 
 function getScaleFromSize(size) {
